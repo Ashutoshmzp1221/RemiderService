b9295c2334124fc12410d01997b1de5e4a2e6bd3
Ashutosh Dwivedi <ashutoshmzp1221@gmail.com>
2025-05-21 17:45:57 +0530

    Added cron job email service

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {PORT} = require('./config/serverConfig');
+const apiRoutes = require('./routes/index');
 
+const jobs = require('./utils/job')
 const setupAndStartServer = () => {
     const app = express();
-
+    
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended : true})); 
-
+    app.use('/api', apiRoutes);
     app.listen(PORT, () => {
         console.log(`The server is start at port ${PORT}`);
+        jobs();
     })
 }
 
